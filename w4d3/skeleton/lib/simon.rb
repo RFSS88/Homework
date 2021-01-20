a24c071b5cf5bafa8c61c5e3dc2df6c24e19f305
rafael silva <silvaserranor@gmail.com>
2021-01-20 10:18:41 -0500

    created draft of Simon game

---
+++ w4d3/skeleton/lib/simon.rb
@@ -0,0 +1,66 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    while !game_over 
+      take_turn
+    end
+      game_over_message
+      reset_game
+  end
+
+  def take_turn
+   
+    unless game_over
+      show_sequence
+      require_sequence
+      round_success_message
+    end
+
+    @sequence_length += 1
+  end
+
+  def show_sequence
+    # print colors in @seq with a pause.
+    # Then clear them from the terminal. 
+    add_random_color
+
+  end
+
+  def require_sequence
+    (0..@sequence_length).times do |i| 
+      p "Play"
+      guess = gets.chomp
+
+      if guess.downcase != @seq[i]
+        game_over = true
+      end
+    end
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+    p "WELL DONE"
+  end
+
+  def game_over_message
+    p "GAME OVER!"
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
