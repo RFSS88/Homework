c0e8b5051a82fe8cbc24faf48f2736185b7e93bb
rafael silva <silvaserranor@gmail.com>
2021-01-25 11:50:37 -0500

    Fixed #add method to check for inclusion

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -1,5 +1,7 @@
+## this is a O(n) implementation of an LRU cache with arrays. 
 class LRUCache
-    def initialize
+    def initialize(size)
+        @size = size
         @cache = []
     end
 
@@ -9,16 +11,41 @@ class LRUCache
     end
 
     def add(el)
-      @cache << [el]
+        if @cache.include?(el)
+            @cache.delete(el)
+            @cache << el
+        elsif
+            count >= @size
+            @cache.shift
+            @cache << el
+        else 
+            @cache << el
+        end
         # adds element to cache according to LRU principle
     end
 
     def show
-       @cache[0] 
+       p @cache 
       # shows the items in the cache, with the LRU item first
     end
-
-    private
-    # helper methods go here!
-
   end
+
+  johnny_cache = LRUCache.new(4)
+
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
+
+  johnny_cache.count # => returns 2
+
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+  johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
